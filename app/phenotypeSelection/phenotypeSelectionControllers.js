cf80faac5213d01c36fcc24596e629e7f2d7bfd0
Jonnalagadda <Varun_Jonnalagadda@AMER.DELL.COM>
2017-04-18 19:36:21 -0700

    Helper functions for predicting outcome of a cross

--- app/phenotypeSelection/phenotypeSelectionControllers.js
+++ app/phenotypeSelection/phenotypeSelectionControllers.js
@@ -1,5 +1,5 @@
 var phenotypeSelectionApp = angular.module("phenotypeSelectionModule", []);
 
 phenotypeSelectionApp.controller("phenotypeSelectionController",['$scope', 'phenotypeFactory', function($scope, phenotypeFactory) {
-    console.log(phenotypeFactory.getPhenotypesList());
+
 }]);
--- app/phenotypeSelection/phenotypeSelectionFactories.js
+++ app/phenotypeSelection/phenotypeSelectionFactories.js
@@ -1,6 +1,6 @@
 phenotypeSelectionApp.factory('phenotypeFactory', [function () {
     return {
-        getPhenotypesList: function () {
+        getPhenotypesList : function () {
             var dataList = [
                 {
                     "phenotype": "Eye Color",
@@ -57,6 +57,49 @@ phenotypeSelectionApp.factory('phenotypeFactory', [function () {
             ];
 
             return dataList;
-        }
+        },
+        prepareForPrediction : function(genotypeOne, genotypeTwo, genotypeThree) {
+            var result = [];
+
+            if(genotypeOne != undefined && genotypeTwo != undefined && genotypeThree != undefined) {
+                for(i=0; i<genotypeOne.length; i++) {
+                    for(j=0; j<genotypeTwo.length; j++) {
+                        for(k=0; k<genotypeThree.length; k++) {
+                            result.push([genotypeOne[i],genotypeTwo[j],genotypeThree[k]]);
+                        }
+                    }
+                }
+            } else if (genotypeOne != undefined && genotypeTwo != undefined) {
+                for(i=0; i<genotypeOne.length; i++) {
+                    for(j=0; j<genotypeTwo.length; j++) {
+                        result.push([genotypeOne[i],genotypeTwo[j]]);
+                    }
+                }
+            } else if (genotypeOne != undefined) {
+                return genotypeOne;
+            }
+
+            return result;
+        },
+        predict : function(predictionSource) {
+
+            var offspring = [];
+            for (i=0; i<predictionSource.length; i++) {
+                for(j=0; j<predictionSource.length; j++) {
+                    if(Array.isArray(predictionSource[i])) {
+                        var outcome = "";
+                        for(k=0; k<predictionSource[i].length; k++) {
+                            outcome = outcome.concat(predictionSource[i][k].concat(predictionSource[j][k]));
+                        }
+                        offspring.push(outcome);
+                    } else {
+                        offspring.push(predictionSource[i].concat(predictionSource[j]));
+                    }
+
+                }
+            }
+
+            return offspring;
+        },
     };
 }]);
